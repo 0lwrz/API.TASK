80ba9d14a19077f7390ce1de8c404ca23fc2c2c5
Sami yassen <samiyaseen970@gmail.com>
2024-01-27 20:47:15 +0300

    API-TASK

---
+++ main.js
@@ -0,0 +1,15 @@
+async function getProducts(){
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    const products = data.products;
+
+    const result = products.map(function(ele){
+        return `
+        <h2>${ele.title}</h2>
+        <img src = "${ele.thumbnail}"/>
+        `;
+    })
+
+    document.querySelector(".products").innerHTML=result;
+};
+getProducts();
